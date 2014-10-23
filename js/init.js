59b428f419d1d35452062498f14e475ba266cf8c
Cameron Hurd <cam.hurd@gmail.com>
2014-10-23 09:54:21 +0100

    better borders

--- js/init.js
+++ js/init.js
@@ -57,10 +57,10 @@ var parallelism = (function($) { var _ = {
 						minRows: 1,
 						
 					// Padding (in px) between items (0 = no padding).
-						padding: 5,
+						padding: 2,
 						
 					// Padding color.
-						paddingColor: '#fff',
+						paddingColor: '#4F4837',
 						
 					// If true, reel scroll will reset on page refresh.
 						resetScroll: true,

85ea5bb822756ff97313c01e19928ea4aaa855b1
Cameron Hurd <cam.hurd@gmail.com>
2014-10-24 09:25:24 +0100

    adding htaccess and about

---
+++ index.php
@@ -0,0 +1,172 @@
+<?php include 'header.php'; ?>
+<body>
+    <div id="wrapper">
+        <div id="main">
+            <div id="reel">
+
+                <!-- Header Item -->
+                <div id="header" class="item" data-width="400">
+                    <div class="inner">
+                        <h1>The Art of Cl&ouml;wnb&auml;b&yuml;</h1>
+                        <p>est. 1991</p>
+                    </div>
+                </div>
+
+                <!-- Thumb Items -->
+                <article class="item thumb" data-width="282">
+                    <h2>Fuck Off</h2>
+                    <a href="images/fulls/fuckoff.jpg">
+                        <img src="images/thumbs/fuckoff.jpg" alt="">
+                    </a>
+                </article>
+
+                <article class="item thumb" data-width="384">
+                    <h2>My Girlfriend's Pets</h2>
+                    <a href="images/fulls/girlfriends_pets.jpg">
+                        <img src="images/thumbs/girlfriends_pets.jpg" alt="">
+                    </a>
+                </article>
+
+                <article class="item thumb" data-width="274">
+                    <h2>Nurse</h2>
+                    <a href="images/fulls/nurse.jpg">
+                        <img src="images/thumbs/nurse.jpg" alt="">
+                    </a>
+                </article>
+
+                <article class="item thumb" data-width="282">
+                    <h2>Ceremony</h2>
+                    <a href="images/fulls/ceremony.jpg">
+                        <img src="images/thumbs/ceremony.jpg" alt="">
+                    </a>
+                </article>
+
+                <article class="item thumb" data-width="476">
+                    <h2>Guitar Girl</h2>
+                    <a href="images/fulls/guitar_girl.jpg">
+                        <img src="images/thumbs/guitar_girl.jpg" alt="">
+                    </a>
+                </article>
+
+                <article class="item thumb" data-width="232">
+                    <h2>Elephant</h2>
+                    <a href="images/fulls/elephant.jpg">
+                        <img src="images/thumbs/elephant.jpg" alt="">
+                    </a>
+                </article>
+
+                <article class="item thumb" data-width="348">
+                    <h2>Lemmy</h2>
+                    <a href="images/fulls/lemmy.jpg">
+                        <img src="images/thumbs/lemmy.jpg" alt="">
+                    </a>
+                </article>
+
+                <article class="item thumb" data-width="348">
+                    <h2>Demon Girl Maybe</h2>
+                    <a href="images/fulls/girlwhoisprettyscaredmaybesheisturningintoademon.jpg">
+                        <img src="images/thumbs/girlwhoisprettyscaredmaybesheisturningintoademon.jpg" alt="">
+                    </a>
+                </article>
+
+                <article class="item thumb" data-width="476">
+                    <h2>Tearing It Up</h2>
+                    <a href="images/fulls/tearingitup.jpg">
+                        <img src="images/thumbs/tearingitup.jpg" alt="">
+                    </a>
+                </article>
+
+                <article class="item thumb" data-width="232">
+                    <h2>Hater</h2>
+                    <a href="images/fulls/hater.jpg">
+                        <img src="images/thumbs/hater.jpg" alt="">
+                    </a>
+                </article>
+
+                <article class="item thumb" data-width="352">
+                    <h2>Hobbit</h2>
+                    <a href="images/fulls/hobbit.jpg">
+                        <img src="images/thumbs/hobbit.jpg" alt="">
+                    </a>
+                </article>
+                
+                <article class="item thumb" data-width="348">
+                    <h2>Guy Who Is Maybe An Elf</h2>
+                    <a href="images/fulls/guywhoismaybeanelf.jpg">
+                        <img src="images/thumbs/guywhoismaybeanelf.jpg" alt="">
+                    </a>
+                </article>
+                
+                <article class="item thumb" data-width="282">
+                    <h2>First Print</h2>
+                    <a href="images/fulls/firstprint.jpg">
+                        <img src="images/thumbs/firstprint.jpg" alt="">
+                    </a>
+                </article>
+                
+                <article class="item thumb" data-width="384">
+                    <h2>Ted's Dead Deck</h2>
+                    <a href="images/fulls/tedsdead.jpg">
+                        <img src="images/thumbs/tedsdead.jpg" alt="">
+                    </a>
+                </article>
+                
+                <article class="item thumb" data-width="274">
+                    <h2>Screamer</h2>
+                    <a href="images/fulls/screamer.jpg">
+                        <img src="images/thumbs/screamer.jpg" alt="">
+                    </a>
+                </article>
+                
+                <article class="item thumb" data-width="282">
+                    <h2>Wu-Tang is for the Children</h2>
+                    <a href="images/fulls/wutang.jpg">
+                        <img src="images/thumbs/wutang.jpg" alt="">
+                    </a>
+                </article>
+            </div>
+        </div>
+        <div id="footer">
+            <div class="left">
+                <p>
+                    <strong>Cl&ouml;wnb&auml;b&yuml;</strong> is Kartigan Kathiravelu, an artist and illustrator based out of Toronto, Canada.
+                    <br />
+                    <a href="#">Get in touch</a>, or read <a href="/about.php">about me</a>.
+                </p>
+            </div>
+            <div class="right">
+                <ul class="contact">
+                    <li>
+                        <a href="https://twitter.com/KART69ClownBaby" class="icon fa-twitter">
+                            <span class="label">Twitter</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://instagram.com/kart69clownbaby" class="icon fa-instagram">
+                            <span class="label">Instagram</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/clownbabyart" class="icon fa-facebook">
+                            <span class="label">Facebook</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.behance.net/clownbaby" class="icon fa-behance">
+                            <span class="label">Dribbble</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="mailto:clownbabyart@gmail.com" class="icon fa-envelope">
+                            <span class="label">Email</span>
+                        </a>
+                    </li>
+                </ul>
+                <ul class="copyright">
+                    <li>&copy; Cl&ouml;wnb&auml;b&yuml; Art 2014</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

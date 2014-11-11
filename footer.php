009f7d444ce5c5ddbe4d5e13ed82e63531b62f72
Cameron Hurd <cam.hurd@gmail.com>
2014-11-11 16:03:52 +0000

    cleaning up mobile style and adding footer

---
+++ footer.php
@@ -0,0 +1,41 @@
+<div id="footer">
+    <div class="left">
+        <p>
+            <strong>Cl&ouml;wnb&auml;b&yuml;</strong> is Kartigan Kathiravelu, an artist and illustrator based out of Toronto, Canada.
+            <br />
+            <a href="#">Get in touch</a>, or read <a href="/about.php">about me</a>.
+        </p>
+    </div>
+    <div class="right">
+        <ul class="contact">
+            <li>
+                <a href="https://twitter.com/KART69ClownBaby" class="icon fa-twitter">
+                    <span class="label">Twitter</span>
+                </a>
+            </li>
+            <li>
+                <a href="http://instagram.com/kart69clownbaby" class="icon fa-instagram">
+                    <span class="label">Instagram</span>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.facebook.com/clownbabyart" class="icon fa-facebook">
+                    <span class="label">Facebook</span>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.behance.net/clownbaby" class="icon fa-behance">
+                    <span class="label">Dribbble</span>
+                </a>
+            </li>
+            <li>
+                <a href="mailto:clownbabyart@gmail.com" class="icon fa-envelope">
+                    <span class="label">Email</span>
+                </a>
+            </li>
+        </ul>
+        <ul class="copyright">
+            <li>&copy; Cl&ouml;wnb&auml;b&yuml; Art 2014</li>
+        </ul>
+    </div>
+</div>

fc5f963eb2d76d8ea93ed45c272249c30a30c9f7
Cameron Hurd <cam.hurd@gmail.com>
2014-11-11 16:05:28 +0000

    adding google analytics

--- footer.php
+++ footer.php
@@ -39,3 +39,9 @@
         </ul>
     </div>
 </div>
+<script>
+    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
